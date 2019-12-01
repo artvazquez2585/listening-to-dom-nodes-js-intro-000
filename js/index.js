99c535f98de6168070d6d06e5d3fda302fd7de07
ARTURO VAZQUEZ CANEDO <art_vazquez_2585@yahoo.com>
2019-12-01 05:33:49 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+//moved function to JS and left event listeners in html
 function bubble(e) {
   // stop! that! propagation!
   e.stopPropagation();
