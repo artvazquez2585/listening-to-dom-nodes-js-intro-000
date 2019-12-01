a51042558117af5471c8b1732647386d03a3235c
ARTURO VAZQUEZ CANEDO <art_vazquez_2585@yahoo.com>
2019-12-01 05:33:19 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,2 +1,8 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+function bubble(e) {
+  // stop! that! propagation!
+  e.stopPropagation();
+
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
